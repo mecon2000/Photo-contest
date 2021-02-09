038efabf96983c4a6f1000af80b43453b7ad7d1d
avivnakar <avivnakar@gmail.com>
2021-02-09 20:04:45 +0200

    added services and scss global file

---
+++ src/services/httpService.js
@@ -0,0 +1,35 @@
+import Axios from 'axios';
+
+const BASE_URL = '/api/'
+
+var axios = Axios.create({
+    witCredentials: true
+});
+
+export const httpService = {
+    get(endpoint, payload) {
+        return ajax(endpoint, 'GET', payload);
+    },
+    post(endpoint, payload) {
+        return ajax(endpoint, 'POST', payload);
+    },
+    put(endpoint, payload) {
+        return ajax(endpoint, 'PUT', payload);
+    },
+    delete(endpoint, payload) {
+        return ajax(endpoint, 'DELETE', payload);
+    }
+};
+async function ajax(endpoint, method, data) {
+    try {
+        const res = await axios({
+            url: `${BASE_URL}${endpoint}`,
+            method,
+            data
+        });
+        return res.data;
+    } catch (err) {
+        console.error`Having hard times at trying to ${method.toLowerCase()} some data... mabye ${endpoint} found your payload offensive, have a look:${data}`
+        throw err
+    }
+}
--- src/services/photoService.js
+++ src/services/photoService.js
@@ -1,6 +1,12 @@
-import axios from 'axios';
-
-export const uploadPhotos (photos) {
-    {photoBlob, }photos[0]
-
+import { httpService } from './httpService';
+const entity = 'photo'
+export const photoService = {
+    uploadPhotos(photos) {
+        // {photoBlob, }photos[0]
+        try {
+            httpService.post(entity, { delitDis: 'now', name: photos });
+        } catch (error) {
+            console.error(error)
+        }
+    }
 }
