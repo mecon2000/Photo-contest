d4f574f81cfe607e767b91e6746427b6d262e32a
Ronnie Ganot <rganot@aligntech.com>
2021-03-30 21:23:24 +0300

    adding to prev commit (showing correct size of files)

--- src/components/FilePreview.js
+++ src/components/FilePreview.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import bytes from "bytes";
+import React from "react";
 
 export function FilePreview(props) {
-    const{file:data}=props
-    const openImageModal = (x) => {}
-    const removeFile = (x) => {}
-    const fileType = (x) => 'jpg';
-    const fileSize = (x) => 1024;
-    const errorMessage = 'Error';
-    console.log(data)
-    return (
-        <div className="file-status-bar">
-            <div onClick={!data.invalid ? () => openImageModal(data) : () => removeFile(data.name)}>
-                <div className="file-type-logo"></div>
-                <div className="file-type">{fileType(data.name)}</div>
-                <span className={`file-name ${data.invalid ? 'file-error' : ''}`}>{data.name}</span>
-                <span className="file-size">({fileSize(data.size)})</span> {data.invalid && <span className='file-error-message'>({errorMessage})</span>}
-            </div>
-            <div className="file-remove" onClick={() => removeFile(data.name)}>X</div>
-        </div>
-    )
+  const { file: data } = props;
+  console.log({ data: data });
+  const openImageModal = (x) => {};
+  const removeFile = (x) => {};
+  const fileType = (x) => data.fileType;
+  const fileSize = (x) => bytes(data.size, {decimalPlaces: 2});  ;
+  const errorMessage = "Error";
+  console.log(data);
+  return (
+    <div className="file-status-bar">
+      <div
+        onClick={
+          !data.invalid
+            ? () => openImageModal(data)
+            : () => removeFile(data.name)
+        }
+      >
+        <div className="file-type-logo"></div>
+        <div className="file-type">{fileType(data.name)}</div>
+        <span className={`file-name ${data.invalid ? "file-error" : ""}`}>
+          {data.name}
+        </span>
+        <span className="file-size">({fileSize(data.size)})</span>{" "}
+        {data.invalid && (
+          <span className="file-error-message">({errorMessage})</span>
+        )}
+      </div>
+      <div className="file-remove" onClick={() => removeFile(data.name)}>
+        X
+      </div>
+    </div>
+  );
 }
-
-
