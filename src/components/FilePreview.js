e6685d73f306333a6b9be16145442f781e0b662d
Ronnie Ganot <rganot@aligntech.com>
2021-04-09 14:00:02 +0300

    showing big photo when clicking on a filePreview. WIP

--- src/components/FilePreview.js
+++ src/components/FilePreview.js
@@ -4,14 +4,22 @@ import { CallbackContext } from "../pages/Upload";
 export function FilePreview(props) {
   const { file: data } = props;
 
-  const removePhoto = useContext(CallbackContext);
+  const { removePhoto, openPhoto } = useContext(CallbackContext);
 
   console.log({ data: data });
-  const openImageModal = (x) => {};
+  const openImageModal = (e) => {
+    openPhoto(data.photoDataUrl);
+  };
+
+  const onRemovePhoto = (e) => {
+    e.stopPropagation();
+    removePhoto(data.name);
+  };
+
   const errorMessage = "Error";
   console.log(data);
   return (
-    <div className="file-preview" onClick={() => openImageModal(data)}>
+    <div className="file-preview" onClick={openImageModal}>
       <img className="thumbnail" src={data.photoDataUrl} alt={data.name} />
       <span className={`file-name ${data.invalid ? "file-error" : ""}`}>
         {data.name}
@@ -22,7 +30,7 @@ export function FilePreview(props) {
       {data.invalid && (
         <span className="file-error-message">({errorMessage})</span>
       )}
-      <button className="file-remove" onClick={() => removePhoto(data.name)}>
+      <button className="file-remove" onClick={onRemovePhoto}>
         remove
       </button>
     </div>
