139fcf30bc8ec349a4fe8d5b6097a8e78f03d2d1
avivnakar <avivnakar@gmail.com>
2021-05-17 15:53:33 +0300

    patch:started show winners, fixed package-lock, added import react

--- src/pages/ShowWinners.js
+++ src/pages/ShowWinners.js
@@ -1,6 +1,17 @@
+import React, { useState } from "react";
+
 export function ShowWinners() {
-    return (
-      <div>
-        <h2>ShowWinners - TBD</h2>
-      </div>
-    )};
+  // photoservice.getWinners()
+  // .then(w=>setWinners(w)
+
+  const [winners, setWinners] = useState(['1', '2', '3']);
+  return (winners.length ? <main className="main-container show-winners">
+    <h2>And The Winner is...</h2>
+    <section className="winners">
+      <div className="second-place">{winners[1]}</div>
+      <div className="first-place">{winners[0]}</div>
+      <div className="third-place">{winners[2]}</div>
+    </section>
+  </main> : <p>waiting for server</p>
+  )
+};
--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import { SmallPhoto } from "../components/SmallPhoto";
 import { downloadAllPhotos } from "../services/photoService";
 
