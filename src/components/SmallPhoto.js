e02f1c66c1aee10c50f54c093a33583f917eeac8
Ronnie Ganot <mecon2000@gmail.com>
2021-05-25 22:10:29 +0300

    added the ability to hide scroe in smallPhoto

--- src/components/SmallPhoto.js
+++ src/components/SmallPhoto.js
@@ -1,25 +1,29 @@
 //should show 1 small photo with 5 stars (to rate)
 export function SmallPhoto(props) {
-  const { src, setScore, score, id } = props;
+  const { src, setScore, score, id, shouldShowScore = true } = props;
 
   return (
     <div className="small-photo">
       <img src={src} alt="alt text" />
-      <div style={{ textAlign: "center" }}>
-        {[...Array(5).keys()].map((i) => {
-          return (
-            <button
-              className={"score-button" + (i+1 === score ? "-selected" : "")}
-              key = {id+i}
-              onClick={(e) => {
-                setScore(i + 1);
-              }}
-            >
-              {i + 1}
-            </button>
-          );
-        })}
-      </div>
+      {shouldShowScore && (
+        <div style={{ textAlign: "center" }}>
+          {[...Array(5).keys()].map((i) => {
+            return (
+              <button
+                className={
+                  "score-button" + (i + 1 === score ? "-selected" : "")
+                }
+                key={id + i}
+                onClick={(e) => {
+                  setScore(i + 1);
+                }}
+              >
+                {i + 1}
+              </button>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
