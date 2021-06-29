8d5cc269080d4240c626de31f6c6e30c10785c25
Ronnie Ganot <mecon2000@gmail.com>
2021-06-29 22:16:37 +0300

    updating of a photo's score, in vote page

--- src/components/SmallPhoto.js
+++ src/components/SmallPhoto.js
@@ -1,11 +1,15 @@
-import React from "react";
-//should show 1 small photo with 5 stars (to rate)
+//should show 1 small photo with/without score (to allow the user to rate)
+import React, {  useState,} from "react";
+import { ModalScreen } from "../components/ModalScreen";
+import { PhotoDetails } from "../components/PhotoDetails";
+
 export function SmallPhoto(props) {
   const { src, setScore, score, id, shouldShowScore = true } = props;
+  const [showPhotoDetails, setShowPhotoDetails] = useState(false);
 
   return (
     <div className="small-photo">
-      <img src={src} alt="alt text" />
+      <img src={src} alt="alt text" onClick={e=>setShowPhotoDetails(true)}/>
       {shouldShowScore && (
         <div style={{ textAlign: "center" }}>
           {[...Array(5).keys()].map((i) => {
@@ -24,6 +28,11 @@ export function SmallPhoto(props) {
             );
           })}
         </div>
+      )}
+      {showPhotoDetails && (
+        <ModalScreen onClick={(e) => setShowPhotoDetails(false)}>
+          <PhotoDetails onClick={(ev)=>ev.stopPropagation()} closePhoto={e=>setShowPhotoDetails(false)} src={src} />
+        </ModalScreen>
       )}
     </div>
   );
