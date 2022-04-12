3906b0135e524131f2028d5c0ea5dfc846acc7b7
mecon2000 <mecon2000@gmail.com>
2022-04-12 22:24:31 +0300

    enabling cors now

--- Server/server-main.js
+++ Server/server-main.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const { connectToDb } = require("./models/dbHandler");
 const app = express();
 const port = 4000;
 
+app.use(cors());
 app.use(require("./controllers/photo"));
 app.use(require("./controllers/contest"));
 
