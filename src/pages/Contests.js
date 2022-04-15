9de3bfbb4257e0e42da998eb28fcfb57fdaab23c
mecon2000 <mecon2000@gmail.com>
2022-04-16 02:10:30 +0300

    confirm before deleting contest

--- src/pages/Contests.js
+++ src/pages/Contests.js
@@ -7,27 +7,28 @@ import uploadImg from "../images/upload.png";
 export function Contests() {
   const [contests, setContests] = useState([]);
   const userId = 1;
+  const stateList = ["uploading", "voting", "show winners"];
+  //TODO use ContestStates (which should be moved to a place accessible by client and server)
 
   useEffect(() => {
     const getAllContests = async () => {
-      const c = await getContests({ userId })
-      setContests(c);      
+      const c = await getContests({ userId });
+      setContests(c);
     };
     getAllContests();
   }, []);
 
   // useEffect; //getcontests
-  // useEffect; //updateContest
   // useEffect; //addContest
-  // useEffect; //deleteContest
   //gotoContest
-
-  const deleteContest = (contestId) => {
-    console.log(`delete ${contestId}`);
-  };
 
   const gotoContest = (contestId) => {
     console.log(`go to ${contestId}`);
+  };
+
+  const handleDeletion = (contestName, contestId) => {
+    if (window.confirm(`Are you sure you want to delete the contes \"${contestName}\"`))
+      deleteContest({ userId, contestId });
   };
 
   return (
@@ -39,14 +40,14 @@ export function Contests() {
               <div className="contest-name">{c.name}</div>
               <MultiToggler
                 initialState={c.state}
-                states={["uploading", "voting", "show winners"]}
-                onClick={(newState) => updateContest({userId, contestId: c._id, newState})}
+                states={stateList}
+                onClick={(newState) => updateContest({ userId, contestId: c._id, newState })}
               />
               <img
                 className="contest-button"
                 src={removeImg}
                 alt="delete contest"
-                onClick={(e) => deleteContest(c._id)}
+                onClick={(e) => handleDeletion(c.name, c._id)}
               />
               <img className="contest-button" src={uploadImg} alt="goto contest" onClick={(e) => gotoContest(c._id)} />
             </div>
